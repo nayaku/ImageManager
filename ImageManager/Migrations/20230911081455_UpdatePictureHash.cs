1698581b276dd88a792eb22ca6ba4d03e4bc9238
nayaku <736248591@qq.com>
2023-09-14 16:45:56 +0800

    MD5为原先数据迁移，不从本地重新获取

--- ImageManager/Migrations/20230911081455_UpdatePictureHash.cs
+++ ImageManager/Migrations/20230911081455_UpdatePictureHash.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 using Shipwreck.Phash;
 using Shipwreck.Phash.Bitmaps;
 using System.Security.Cryptography;
+using System.Text;
 
 #nullable disable
 
@@ -38,25 +39,15 @@ namespace ImageManager.Migrations
             // 更新图片的Hash
             foreach (var picture in context.Pictures)
             {
-                var filePath = Path.Join(picture.ImageFolderPath, picture.Path);
-                using var fs = File.OpenRead(filePath);
-                using var reader = new BufferedStream(fs, 16 * 1024 * 1024); //16MB
-                var md5 = MD5.HashData(reader);
+                // md5
+                var md5String = Encoding.ASCII.GetString(picture.Hash);
+                var md5 = Convert.FromHexString(md5String);
                 picture.Hash = md5;
-                // 读取图片
-                reader.Seek(0, SeekOrigin.Begin);
-                var fif = FreeImageAPI.FreeImage.GetFileTypeFromStream(reader);
-                var fibitmap = FreeImageAPI.FreeImage.LoadFromStream(reader, FreeImageAPI.FREE_IMAGE_LOAD_FLAGS.DEFAULT, ref fif);
-                // 判断是否为位图
-                if (FreeImageAPI.FreeImage.GetImageType(fibitmap) != FreeImageAPI.FREE_IMAGE_TYPE.FIT_BITMAP)
-                {
-                    var newBitmap = FreeImageAPI.FreeImage.ConvertToType(fibitmap, FreeImageAPI.FREE_IMAGE_TYPE.FIT_BITMAP, true);
-                    FreeImageAPI.FreeImage.Unload(fibitmap);
-                    fibitmap = newBitmap;
-                }
-                // 转换为GDI+图片
-                using var bitmap = FreeImageAPI.FreeImage.GetBitmap(fibitmap);
-                FreeImageAPI.FreeImage.Unload(fibitmap);
+                
+                // phash
+                var filePath = Path.Join(picture.ImageFolderPath, picture.Path);
+                var fif = FreeImageAPI.FREE_IMAGE_FORMAT.FIF_UNKNOWN;
+                using var bitmap = FreeImageAPI.FreeImage.LoadBitmap(filePath, FreeImageAPI.FREE_IMAGE_LOAD_FLAGS.DEFAULT, ref fif);
                 // 相似判断
                 var phash = ImagePhash.ComputeDigest(bitmap.ToLuminanceImage()).Coefficients;
                 picture.WeakHash = phash;
