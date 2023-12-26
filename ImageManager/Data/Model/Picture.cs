3a694f43e0fe34ce47e59d1e6c2dbd75f2c037e2
nayaku <736248591@qq.com>
2023-12-26 22:57:35 +0800

    优化性能

--- ImageManager/Data/Model/Picture.cs
+++ ImageManager/Data/Model/Picture.cs
@@ -6,17 +6,6 @@ using System.Windows.Media.Imaging;
 
 namespace ImageManager.Data.Model
 {
-    public struct Size
-    {
-        public int Width { get; set; }
-        public int Height { get; set; }
-        public Size() { }
-        public Size(int width, int height)
-        {
-            Width = width;
-            Height = height;
-        }
-    }
     public enum PictureAddStateEnum
     {
         WaitToAdd,
