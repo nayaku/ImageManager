2cc2b7cd1115665888d30a37076a7841fc45a9da
nayaku <736248591@qq.com>
2026-04-15 16:16:24 +0800

    包装添加图片的耗时错误

--- ImageManager/ViewModels/AddImageViewModel.cs
+++ ImageManager/ViewModels/AddImageViewModel.cs
@@ -73,22 +73,25 @@ namespace ImageManager.ViewModels
             try
             {
                 var toAddPictures = Pictures.Where(p => p.AcceptToAdd).ToList();
-                toAddPictures.ForEach(p =>
+                await Task.Run(() =>
                 {
-                    // 由于文件被占用删不掉，所以只能复制到新的位置，之后再删除原文件
-                    var tempDir = p.ImageFolderPath;
-                    p.SetDefaultImageFolderPath();
-                    var file = Path.Combine(tempDir, p.Path);
-                    var newFile = Path.Combine(p.ImageFolderPath, p.Path);
-                    File.Copy(file, newFile);
-                    if (p.ThumbnailPath != null)
+                    toAddPictures.ForEach(p =>
                     {
-                        var thumbnailFile = Path.Combine(tempDir, p.ThumbnailPath);
-                        var newThumbnailFile = Path.Combine(p.ImageFolderPath, p.ThumbnailPath);
-                        File.Copy(thumbnailFile, newThumbnailFile);
-                    }
+                        // 由于文件被占用删不掉，所以只能复制到新的位置，之后再删除原文件
+                        var tempDir = p.ImageFolderPath;
+                        p.SetDefaultImageFolderPath();
+                        var file = Path.Combine(tempDir, p.Path);
+                        var newFile = Path.Combine(p.ImageFolderPath, p.Path);
+                        File.Copy(file, newFile);
+                        if (p.ThumbnailPath != null)
+                        {
+                            var thumbnailFile = Path.Combine(tempDir, p.ThumbnailPath);
+                            var newThumbnailFile = Path.Combine(p.ImageFolderPath, p.ThumbnailPath);
+                            File.Copy(thumbnailFile, newThumbnailFile);
+                        }
+                    });
+                    _context.Pictures.AddRange(toAddPictures);
                 });
-                _context.Pictures.AddRange(toAddPictures);
                 await _context.SaveChangesAsync();
                 _successEvent?.Invoke(this, toAddPictures.Count);
             }
