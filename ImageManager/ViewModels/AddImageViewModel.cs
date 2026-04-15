aeebf8d3cc31b269afcf5475dd12126b6ae81ecd
nayaku <736248591@qq.com>
2026-04-15 15:54:39 +0800

    修复添加文件文件复制失败导致等待对话框永久卡死的问题

--- ImageManager/ViewModels/AddImageViewModel.cs
+++ ImageManager/ViewModels/AddImageViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using HandyControl.Controls;
 using ImageManager.Data;
 using ImageManager.Data.Model;
+using ImageManager.Logging;
 using ImageManager.Views;
 using StyletIoC;
+using System.Windows;
 
 namespace ImageManager.ViewModels
 {
@@ -21,6 +23,7 @@ namespace ImageManager.ViewModels
         private string _message;
         private EventHandler<int> _successEvent;
         private ImageContext _context;
+        private Logger _logger = LoggerFactory.GetLogger(nameof(AddImageViewModel));
 
 
         public AddImageViewModel(List<Picture> pictures, string message, EventHandler<int> successEvent, ImageContext context)
@@ -67,33 +70,47 @@ namespace ImageManager.ViewModels
         {
             _isWorking = true;
             var dialog = Dialog.Show(new WaitingDialog());
-            var count = await Task.Run(() =>
+            int count = 0;
+            try
             {
-                var count = 0;
-                Pictures.ForEach(p =>
+                count = await Task.Run(() =>
                 {
-                    // 由于文件被占用删不掉
-                    if (p.AcceptToAdd)
+                    var count = 0;
+                    Pictures.ForEach(p =>
                     {
-                        var tempDir = p.ImageFolderPath;
-                        p.SetDefaultImageFolderPath();
-                        var file = Path.Combine(tempDir, p.Path);
-                        var newFile = Path.Combine(p.ImageFolderPath, p.Path);
-                        File.Copy(file, newFile);
-                        if (p.ThumbnailPath != null)
+                        // 由于文件被占用删不掉
+                        if (p.AcceptToAdd)
                         {
-                            var thumbnailFile = Path.Combine(tempDir, p.ThumbnailPath);
-                            var newThumbnailFile = Path.Combine(p.ImageFolderPath, p.ThumbnailPath);
-                            File.Copy(thumbnailFile, newThumbnailFile);
+                            var tempDir = p.ImageFolderPath;
+                            p.SetDefaultImageFolderPath();
+                            var file = Path.Combine(tempDir, p.Path);
+                            var newFile = Path.Combine(p.ImageFolderPath, p.Path);
+                            File.Copy(file, newFile);
+                            if (p.ThumbnailPath != null)
+                            {
+                                var thumbnailFile = Path.Combine(tempDir, p.ThumbnailPath);
+                                var newThumbnailFile = Path.Combine(p.ImageFolderPath, p.ThumbnailPath);
+                                File.Copy(thumbnailFile, newThumbnailFile);
+                            }
+                            _context.Add(p);
+                            count++;
                         }
-                        _context.Add(p);
-                        count++;
-                    }
+                    });
+                    _context.SaveChanges();
+                    return count;
                 });
-                _context.SaveChanges();
-                return count;
-            });
-            dialog.Close();
+            }
+            catch (Exception ex)
+            {
+                DialogViewModel.Show(Container, "添加失败", ex.Message, "确定");
+                _logger.Error(ex);
+                return;
+            }
+            finally
+            {
+                dialog.Close();   // 无论成功还是失败都关闭
+                _isWorking = false;
+            }
             _canClose = true;
             Result = true;
             RequestClose();
