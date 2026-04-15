8dec9d80b3bf181700037b3145be1201c28b315e
nayaku <736248591@qq.com>
2026-04-15 18:03:47 +0800

    Bitmap 未 Dispose

--- ImageManager/Tools/ImageUtility.cs
+++ ImageManager/Tools/ImageUtility.cs
@@ -35,7 +35,7 @@ namespace ImageManager.Tools
             BitmapEncoder enc = new BmpBitmapEncoder();
             enc.Frames.Add(BitmapFrame.Create(bitmapImage));
             enc.Save(outStream);
-            var bitmap = new Bitmap(outStream);
+            using var bitmap = new Bitmap(outStream);
 
             return new Bitmap(bitmap);
         }
